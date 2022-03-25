cfcdc6c5779366d42199abfeb19e86c94db8aacc
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-25 15:43:59 -0400

    adding style to page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CharacterCard from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [cards , setCards] = useState()
+  const [cards , setCards] = useState();
   
 
   useEffect(() => {
@@ -12,12 +12,10 @@ const App = () => {
     .then(resp => {
   //  console.log(resp.data.results)
   setCards(resp.data.results)
-  
     })
     .catch(err => {console.log(err)})
   }, [])   
-
-
+ 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -25,7 +23,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>  
-    {cards && <CharacterCard content={cards}></CharacterCard>}
+    {cards && <CharacterCard content={cards} ></CharacterCard>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,28 +3,32 @@ import React from "react";
 import { Alert } from 'reactstrap';
 import styled from 'styled-components'
 
-const Characters = styled.li`
-
+const Characters = styled.span`
+color:black;
+`
+const CharacterDescription = styled.ul`
+ list-style:none;
+ display:flex;
+ justify-content:space-around;
+ color:red;
+ font-size:24px;
 `
 
 function CharacterCard(props){
     console.log(props)
     return(
    <div>
-        <ul>
             {props.content.map((element, index) => {
               return <Characters key={index}>
               <h2>{element.name}</h2> 
-            <ul>
-             <li> {element["birth_year"]}</li>
+            <CharacterDescription>
+             <li>{`Birth Date:` + element["birth_year"]}</li>
              <li> {element["eye_color"]}</li>
              <li> {element["skin_color"]}</li>
              <li> {element.height}</li>
-            </ul>
-            {element.starships}
+            </CharacterDescription>
              </Characters> 
             })}
-        </ul>
    </div>
     )
 }
