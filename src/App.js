f5d03f2a3d15e67acbcddbcd6ca9e492d0a459d1
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-25 14:41:38 -0400

    creating structure for charectersJS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, {useState, useEffect} from 'react';
-
+import axios from 'axios';
+import CharacterCard from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [cards , setCards] = useState()
+  
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+    .then(resp => {
+  //  console.log(resp.data.results)
+  setCards(resp.data.results)
+  
+    })
+    .catch(err => {console.log(err)})
+  }, [])   
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -10,7 +24,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Characters</h1>  
+    {cards && <CharacterCard content={cards}></CharacterCard>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from "react";
+import { Alert } from 'reactstrap';
+import styled from 'styled-components'
+
+const Characters = styled.li`
+
+`
+
+function CharacterCard(props){
+    console.log(props)
+    return(
+   <div>
+        <ul>
+            {props.content.map((element, index) => {
+              return <Characters key={index}>{element.name},{element["birth_year"]},{element["eye_color"]},{element["skin_color"]},{element.height}</Characters> 
+            })}
+        </ul>
+   </div>
+    )
+}
+
+export default CharacterCard;
